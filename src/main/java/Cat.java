89aef04a3b2b2ba6b096b492e9190bb60f902690
Alexey Gorbunov <alexgorbunov95@mail.ru>
2024-03-16 14:10:33 +0300

    Cat

---
+++ src/main/java/Cat.java
@@ -0,0 +1,36 @@
+import java.util.Objects;
+
+public class Cat {
+    private String message;
+
+    public Cat() {
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return Objects.equals(message, cat.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(message);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
